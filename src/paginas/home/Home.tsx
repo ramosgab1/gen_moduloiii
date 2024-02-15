3d32ff4e5ef07f8eced43b6f3260b2a375feafc2
ramosgab1 <gabrieller_david@hotmail.com>
2024-02-15 10:58:12 -0300

    Atividade 14/02/2024

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,12 @@
+import './Home.css'
+
+function Home (){ 
+ return (
+ <>
+    <h1 className="titulo">Ablueblueblueblue.</h1>
+    <img className="img" src="https://3fc4ed44-3fbc-419a-97a1-a29742511391.selcdn.net/coub_storage/coub/simple/cw_timeline_pic/2625f583d67/08fa88f7e0cdb7a9615c7/1470711061_image.jpg" alt="Murdoooc"></img>
+</>
+ )
+}
+
+export default Home;
